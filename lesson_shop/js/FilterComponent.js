a3b7f78a114d5df40f545901fc9bf8c1332a694b
AnnaVishnyakova <simonya6ka@gmail.com>
2021-11-11 12:43:46 +0300

    hw6 full version

--- lesson_shop/js/FilterComponent.js
+++ lesson_shop/js/FilterComponent.js
@@ -1,12 +1,12 @@
 Vue.component('search', {
-    data() {
-        return {
-            userSearch: ''
-        }
-    },
+    // data() {
+    //     return {
+    //         userSearch: ''
+    //     }
+    // },
   
-    template: `<form action="#" class="header__search-form" @submit.prevent='$parent.filter(userSearch)'>
-                    <input type="text" class="header__search-field" v-model="userSearch">
+    template: `<form action="#" class="header__search-form" @submit.prevent='$parent.filter'>
+                    <input type="text" class="header__search-field" v-model="$parent.userSearch">
                     <button class="btn-search" type="submit">
                         <i class="fas fa-search"></i>
                     </button>
